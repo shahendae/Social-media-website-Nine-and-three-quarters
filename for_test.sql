8fa4b5b9d780ca90e998458ce377914ded13c17e
{Shahenda <{shahenda.mena@gmail.com}>
2019-07-28 17:38:55 +0200

    Done

---
+++ for_test.sql
@@ -0,0 +1,4 @@
+insert into friend_request (send_id, recieve_id) values('1','3');
+insert into friend_request (send_id, recieve_id) values('2','3');
+insert into friend_request (send_id, recieve_id) values('4','3');
+insert into friend_request (send_id, recieve_id) values('5','3');
